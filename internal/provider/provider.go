fb698ae7e6a258215b389699fc7a3d70a7317636
Fuochi <ffuochi@hotmail.com>
2023-01-27 12:20:50 +0400

    feat: add quality definitions data source

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -285,6 +285,7 @@ func (p *WhisparrProvider) DataSources(ctx context.Context) []func() datasource.
 		NewDelayProfilesDataSource,
 		NewQualityProfileDataSource,
 		NewQualityProfilesDataSource,
+		NewQualityDefinitionsDataSource,
 
 		// System Status
 		NewSystemStatusDataSource,
