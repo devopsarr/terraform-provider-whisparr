6cb9ee490563e497a0c4cb3f129545774a64b4f1
Fuochi <ffuochi@hotmail.com>
2023-01-27 09:27:58 +0400

    feat: add import list tmdb user resource

---
+++ internal/provider/import_list_tmdb_user_resource_test.go
@@ -0,0 +1,59 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccImportListTMDBUserResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				PreConfig: rootFolderDSInit,
+				Config:    testAccImportListTMDBUserResourceConfig("resourceTMDUserTest", "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("whisparr_import_list_tmdb_user.test", "should_monitor", "true"),
+					resource.TestCheckResourceAttrSet("whisparr_import_list_tmdb_user.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccImportListTMDBUserResourceConfig("resourceTMDUserTest", "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("whisparr_import_list_tmdb_user.test", "should_monitor", "false"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "whisparr_import_list_tmdb_user.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccImportListTMDBUserResourceConfig(name, monitor string) string {
+	return fmt.Sprintf(`
+	resource "whisparr_import_list_tmdb_user" "test" {
+		enabled = false
+		enable_auto = false
+		search_on_add = false
+		root_folder_path = "/config"
+		should_monitor = %s
+		minimum_availability = "tba"
+		quality_profile_id = 1
+		name = "%s"
+		account_id = "11842"
+		user_list_type = 1
+		access_token = "Token"
+	}`, monitor, name)
+}
