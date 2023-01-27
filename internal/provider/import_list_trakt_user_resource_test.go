9c0c2d8c15b8a6e1d8f349e9ddc2b779ef0bc161
Fuochi <ffuochi@hotmail.com>
2023-01-27 09:24:33 +0400

    feat: add import list trakt user resource

---
+++ internal/provider/import_list_trakt_user_resource_test.go
@@ -0,0 +1,60 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccImportListTraktUserResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				PreConfig: rootFolderDSInit,
+				Config:    testAccImportListTraktUserResourceConfig("resourceTraktUserTest", "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("whisparr_import_list_trakt_user.test", "should_monitor", "true"),
+					resource.TestCheckResourceAttrSet("whisparr_import_list_trakt_user.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccImportListTraktUserResourceConfig("resourceTraktUserTest", "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("whisparr_import_list_trakt_user.test", "should_monitor", "false"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "whisparr_import_list_trakt_user.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccImportListTraktUserResourceConfig(name, monitor string) string {
+	return fmt.Sprintf(`
+	resource "whisparr_import_list_trakt_user" "test" {
+		enabled = false
+		enable_auto = false
+		search_on_add = false
+		root_folder_path = "/config"
+		should_monitor = %s
+		minimum_availability = "tba"
+		quality_profile_id = 1
+		name = "%s"
+		auth_user = "User1"
+		access_token = "Token"
+		trakt_list_type = 0
+		limit = 100
+	}`, monitor, name)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -170,6 +170,7 @@ func (p *WhisparrProvider) Resources(ctx context.Context) []func() resource.Reso
 
 		// Import Lists
 		NewImportListResource,
+		NewImportListTraktUserResource,
 
 		// Media Management
 		NewMediaManagementResource,
