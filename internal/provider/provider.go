3ab49ec7ba824880bbd60ee9499074880a304d0d
Fuochi <ffuochi@hotmail.com>
2023-01-27 09:33:21 +0400

    feat: add import list imdb resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -170,6 +170,7 @@ func (p *WhisparrProvider) Resources(ctx context.Context) []func() resource.Reso
 
 		// Import Lists
 		NewImportListResource,
+		NewImportListIMDBResource,
 		NewImportListPlexResource,
 		NewImportListRSSResource,
 		NewImportListStevenluResource,
