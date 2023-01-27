5c677461fe7106a0e1f2321d7eff272ed76d68e2
Fuochi <ffuochi@hotmail.com>
2023-01-27 09:29:18 +0400

    feat: add import list tmdb list resource

---
+++ examples/resources/whisparr_import_list_tmdb_list/resource.tf
@@ -0,0 +1,11 @@
+resource "whisparr_import_list_tmdb_list" "example" {
+  enabled              = true
+  enable_auto          = false
+  search_on_add        = false
+  root_folder_path     = "/config"
+  should_monitor       = true
+  minimum_availability = "tba"
+  quality_profile_id   = 1
+  name                 = "Example"
+  list_id              = "11842"
+}
